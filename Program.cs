26d7d1c991ad4856ed573bf644b79a635c7aa5cf
lune200 <nekomoon2003@gmail.com>
2023-05-26 20:11:02 -0300

    v\final

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double glicemia; 
+Console.WriteLine ("Insira o valor do seu teste de glicemia:");
+
+glicemia = Convert.ToDouble (Console.ReadLine ());
+
+
+if (glicemia <92)
+Console.WriteLine ("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+
+else if (glicemia > 92 && glicemia < 126) 
+Console.WriteLine("Compatível com Diabetes Gestacional");
+
+else if (glicemia > 126)
+Console.WriteLine ("Compativel com Diabetes Mellitus na Gravidez");
